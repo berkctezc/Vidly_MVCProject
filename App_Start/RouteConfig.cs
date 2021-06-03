c5e9b034a1bcd98554f1b0b542504485fd98c390
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-03 23:44:58 +0300

    routing and passing data to config

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,6 +13,16 @@ namespace Vidly_MVCProject
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //attribute routing
+            routes.MapMvcAttributeRoutes();
+            ////convention based routing
+            /// custom route
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action = "ByReleaseDate" },
+            //    new { year = @"2020|2021", month = @"\d{2}" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Security.Cryptography;
+using System.Web.Mvc;
 using Vidly_MVCProject.Models;
 
 namespace Vidly_MVCProject.Controllers
@@ -8,9 +10,42 @@ namespace Vidly_MVCProject.Controllers
         // GET: Movies
         public ActionResult Random()
         {
-            var movie = new Movie() {Name="Shrek!" };
+            var movie = new Movie() { Name = "Shrek!" };
+
+            //passing data do view
+            ViewData["Movie"] = movie;
+            ViewBag.Movie = movie;
+
 
             return View(movie);
+            
+            
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {page="1",sortBy="name"});
+        }
+
+        public ActionResult Edit(int? id)
+        {
+           // Random random = new Random();
+            if (!id.HasValue) id = new Random().Next(100);
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(string.Format("PageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year:regex(\\d{4}:range(1800,2021))}/{month:regex(\\d{2}:range(1,12))}")]
+        public ActionResult ByReleaseDate(int year,int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
