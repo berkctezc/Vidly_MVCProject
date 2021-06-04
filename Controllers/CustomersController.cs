9d7b42ad63dec69b38c0d24ee2c6d8bb5c586b59
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-04 21:42:20 +0300

    queried data from db

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -7,33 +7,33 @@ namespace Vidly_MVCProject.Controllers
 {
     public class CustomersController : Controller
     {
+        private Context _context;
+
+        public CustomersController()
+        {
+            _context = new Context();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().Single(c => c.Id == id);
+            var customer = _context.Customers.Single(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
-        }
-
-        private List<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer{Name="Orson Welles", Id =1},
-                new Customer{Name="Alfred Hitchcock", Id =2},
-                new Customer{Name="Akira Kurosawa", Id =3},
-                new Customer{Name="Martin Scorsese", Id =4},
-                new Customer{Name="Stanley Kubrick", Id =5},
-            };
         }
     }
 }
