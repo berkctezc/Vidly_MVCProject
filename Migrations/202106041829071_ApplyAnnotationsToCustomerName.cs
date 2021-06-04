5ca8ca13fca9eac1f92845486d214eb2301fe142
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-04 21:30:04 +0300

    applied constraints to customer model

---
+++ Migrations/202106041829071_ApplyAnnotationsToCustomerName.cs
@@ -0,0 +1,18 @@
+﻿namespace Vidly_MVCProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ApplyAnnotationsToCustomerName : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String());
+        }
+    }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,8 +1,12 @@
-﻿namespace Vidly_MVCProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly_MVCProject.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
