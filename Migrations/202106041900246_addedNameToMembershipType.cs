a2851a9a125f48a85ec297ef3aeadc51d79f0152
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-04 22:23:36 +0300

    sec3-ex1

---
+++ Migrations/202106041900246_addedNameToMembershipType.cs
@@ -0,0 +1,18 @@
+﻿namespace Vidly_MVCProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedNameToMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "Name", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.MembershipTypes", "Name");
+        }
+    }
+}
---
+++ Migrations/202106041903064_updateNameValuesMembershipTypes.cs
@@ -0,0 +1,21 @@
+﻿namespace Vidly_MVCProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateNameValuesMembershipTypes : DbMigration
+    {
+        public override void Up()
+        {
+
+            Sql("Update MembershipTypes set name='Pay as you go' where id=1");
+            Sql("Update MembershipTypes set name='Monthly' where id=2");
+            Sql("Update MembershipTypes set name='Quarterly' where id=3");
+            Sql("Update MembershipTypes set name='Annually' where id=4");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
