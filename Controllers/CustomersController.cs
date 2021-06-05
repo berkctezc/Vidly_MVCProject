92d47cb91ae0322b2bffb8082821f04a106ef85e
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-05 22:14:10 +0300

    validation, styling validation, overriding validation message

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -51,6 +51,16 @@ namespace Vidly_MVCProject.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+
             if(customer.Id==0) 
                 _context.Customers.Add(customer);
             else
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -7,7 +7,7 @@ namespace Vidly_MVCProject.Models
     {
         public int Id { get; set; }
         
-        [Required]
+        [Required(ErrorMessage ="Please enter customer's name.")]
         [StringLength(255)]
         
         public string Name { get; set; }
