52b4a626930a93c13eaa0f16f7803015f3f5b910
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-06 17:05:06 +0300

    installed bootbox, downgraded jquery

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -19,7 +19,10 @@ namespace Vidly_MVCProject
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js",
+                      "~/Scripts/respond.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
