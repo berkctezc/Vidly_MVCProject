11dcc59ce7dd7fcb0d986c9b07016310bf8fe1d7
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-06 18:41:59 +0300

    installed and implemented datatables on customers

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -7,8 +7,14 @@ namespace Vidly_MVCProject
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js", 
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootbox.js", 
+                        "~/Scripts/respond.js",
+                        "~/Scripts/DataTables/jquery.dataTables.js",
+                        "~/Scripts/DataTables/dataTables.bootstrap.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -18,14 +24,10 @@ namespace Vidly_MVCProject
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootbox.js",
-                      "~/Scripts/respond.js"
-                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/content/DataTables/css/dataTables.bootstrap.css",
                       "~/Content/site.css"));
         }
     }
