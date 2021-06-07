0083cf692e4367e94b60dfe1a144867374b4fead
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-06-07 20:28:57 +0300

    edge cases

--- Controllers/Api/NewRentalsController.cs
+++ Controllers/Api/NewRentalsController.cs
@@ -15,21 +15,35 @@ namespace Vidly_MVCProject.Controllers.Api
 
         public NewRentalsController()
         {
-            _appDbContext =new AppDbContext();
+            _appDbContext = new AppDbContext();
         }
 
 
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            var customer = _appDbContext.Customers.Single(
+            var customer = _appDbContext.Customers.SingleOrDefault(
                 c => c.Id == newRental.CustomerId);
 
+            if (customer == null)
+                return BadRequest("CustomerId is not valid.");
+
+            if (newRental.MovieIds.Count==0)
+                return BadRequest("No Movie Ids have been given.");
+
+
             var movies = _appDbContext.Movies.Where(
-                m => newRental.MovieIds.Contains(m.Id));
+                m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            if (movies.Count!=newRental.MovieIds.Count)
+                return BadRequest("One or more MovieIds are invalid.");
+
 
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable==0)
+                    return BadRequest("Movie is not available");
+
                 movie.NumberAvailable--;
 
                 var rental = new Rental()
